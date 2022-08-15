1e6b06881a0d5167b242e58a5a004a7bb0009842
juanpmz2001 <70351734+juanpmz2001@users.noreply.github.com>
2022-08-15 17:41:43 -0500

    nuevo metodo de jugar funcional, antes de implementar la IA

--- Main.py
+++ Main.py
@@ -1,8 +1,49 @@
 import pygame
 import sys
 from math import *
-from NodeChain import NodeChain
-from Treess import Treesses
+from time import sleep
+#from NodeChain import NodeChain
+#import Tree 
+
+# Initialization of Pygame
+#close()
+pygame.init()
+clock = pygame.time.Clock()
+
+# Initializing the Screen
+horizontal_blocks=6
+vertical_blocks=11
+blocks = 40
+width = blocks*horizontal_blocks
+height = blocks*vertical_blocks
+display = pygame.display.set_mode((width, height))
+
+# Colors
+background = (21, 67, 96)
+border = (208, 211, 212)
+red = (231, 76, 60)
+white = (244, 246, 247)
+violet = (136, 78, 160)
+yellow = (244, 208, 63)
+green = (88, 214, 141)
+
+#Selected players
+playerColor = [red, green]
+noPlayers = 2
+font = pygame.font.SysFont("Times New Roman", 20)
+#title of the game window 
+pygame.display.set_caption("Chain Reaction %d Player, Isis vs AI" % noPlayers)
+#initialize players and scores
+score = []
+players = []
+for i in range(noPlayers):
+    score.append(0)
+    players.append(playerColor[i])
+
+d = blocks//2 - 2
+cols = int(width//blocks)
+rows = int(height//blocks)
+grid = []
 
 
 # print the grid state
@@ -10,13 +51,11 @@ def printer():
     for x in grid:
         for obj in x:
             print(obj.__str__())
-
 # Quit or Close the Game Window
 def close():
     #printer()
     pygame.quit()
     sys.exit()
-
 # Class for Each Spot in Grid
 class Spot():
     def __init__(self):
@@ -36,7 +75,6 @@ class Spot():
 
     def __str__(self):
         return f"{self.color}: {self.noAtoms}"
-
 # Initializing the Grid with "Empty or 0"
 def initializeGrid():
     global grid, score, players
@@ -54,7 +92,6 @@ def initializeGrid():
     for i in range(cols):
         for j in range(rows):
             grid[int(i)][int(j)].addNeighbors(i, j)
-
 # Draw the Grid in Pygame Window   
 def drawGrid(currentIndex):
     r = 0
@@ -64,7 +101,6 @@ def drawGrid(currentIndex):
         c += blocks
         pygame.draw.line(display, players[currentIndex], (c, 0), (c, height))
         pygame.draw.line(display, players[currentIndex], (0, r), (width, r))
-
 # Draw the Present Situation of Grid
 def showPresentGrid(vibrate = 1):
     r = -blocks
@@ -95,37 +131,6 @@ def showPresentGrid(vibrate = 1):
                 pygame.draw.ellipse(display, grid[int(i)][int(j)].color, (x - vibrate, y, d, d))
 
     pygame.display.update()
-
-# Increase the Atom when Clicked
-def addAtom(i, j, color):
-    grid[int(i)][int(j)].noAtoms += 1
-    grid[int(i)][int(j)].color = color
-    if grid[int(i)][int(j)].noAtoms >= len(grid[int(i)][int(j)].neighbors):
-        overFlow(grid[int(i)][int(j)], color)
-    
-# Split the Atom when it Increases the "LIMIT"
-def overFlow(cell, color):
-    showPresentGrid()
-    cell.noAtoms = 0
-    for m in range(len(cell.neighbors)):
-        cell.neighbors[m].noAtoms += 1
-        cell.neighbors[m].color = color
-        if cell.neighbors[m].noAtoms >= len(cell.neighbors[m].neighbors):
-            overFlow(cell.neighbors[m], color)
-
-# Checking if Any Player has WON!
-def isPlayerInGame():
-    global score
-    playerScore = []
-    for i in range(noPlayers):
-        playerScore.append(0)
-    for i in range(cols):
-        for j in range(rows):
-            for k in range(noPlayers):
-                if grid[int(i)][int(j)].color == players[k]:
-                    playerScore[k] += grid[int(i)][int(j)].noAtoms
-    score = playerScore[:]
-
 # GAME OVER
 def gameOver(playerIndex):
     while True:
@@ -136,18 +141,17 @@ def gameOver(playerIndex):
                 if event.key == pygame.K_q:
                     close()
                 if event.key == pygame.K_r:
-                    gameLoop()
+                    game2()
 
-        text = font.render("Player %d Won!" % (playerIndex + 1), True, white)
+        text = font.render(f"Player {playerIndex} Won!" , True, white)
         text2 = font.render("Press \'r\' to Reset!", True, white)
 
         display.blit(text, ((width-160)/2, height/3))
-        display.blit(text2, ((width-200)/2, height/2 ))
+        display.blit(text2, ((width-160)/2, height/2 ))
 
         pygame.display.update()
         clock.tick(60)
 
-#
 def checkWon():
     num = 0
     for i in range(noPlayers):
@@ -160,16 +164,40 @@ def checkWon():
 
     return 9999
 
-# Main Loop
-def gameLoop():
+def gridround ():
+    win = None
+    newCellsToExplote = 0
+    for j,col in enumerate(grid):
+        for i,cell in enumerate(col):
+            #check if the cell needs to explote, in that case add an atom to their neighbors
+            if cell.noAtoms >= len(cell.neighbors):
+                cell.noAtoms = cell.noAtoms-len(cell.neighbors)
+                for m in range(len(cell.neighbors)):
+                    cell.neighbors[m].noAtoms += 1
+                    cell.neighbors[m].color = cell.color
+                    if cell.neighbors[m].noAtoms >= len(cell.neighbors[m].neighbors):
+                        newCellsToExplote+=1
+    return grid,newCellsToExplote
+
+def checkwin():
+    firstcolor=None
+    for col in grid:
+        for cell in col:
+            if cell.noAtoms!=0:
+                if firstcolor==None:
+                    firstcolor=cell.color
+                if firstcolor!=cell.color:
+                    return False
+    return True
+          
+def game2():
     initializeGrid()
     loop = True
-
     turns = 0
-    
     currentPlayer = 0
-
     vibrate = .5
+    global grid
+
 
     while loop:
         for event in pygame.event.get():
@@ -182,96 +210,51 @@ def gameLoop():
                 x, y = pygame.mouse.get_pos()
                 i = x/blocks
                 j = y/blocks
-                if grid[int(i)][int(j)].color == players[int(currentPlayer)] or grid[int(i)][int(j)].color == border:
+                if grid[int(i)][int(j)].color == players[int(currentPlayer)] or grid[int(i)][int(j)].noAtoms == 0:
                     turns += 1
-                    addAtom(i, j, players[currentPlayer])
+                    newCellsToExplote=1
+                    grid[int(i)][int(j)].noAtoms += 1
+                    grid[int(i)][int(j)].color = players[currentPlayer]
+                    while (newCellsToExplote>0):
+                        grid,newCellsToExplote = gridround()
+                        display.fill(background)
+                        """"# Vibrate the Atoms in their Cells
+                        vibrate *= -1
+                        drawGrid(currentPlayer)
+                        showPresentGrid(vibrate)
+                        pygame.display.update()"""
+                        if turns>1 and checkwin():
+                            if currentPlayer == 0:
+                                gameOver("Red")
+                            else:
+                                gameOver("Green")
                     currentPlayer += 1
                     if currentPlayer >= noPlayers:
                         currentPlayer = 0
-                    
-                if turns >= noPlayers:
-                    isPlayerInGame()
-                
-        
+
         display.fill(background)
         # Vibrate the Atoms in their Cells
         vibrate *= -1
-        
         drawGrid(currentPlayer)
         showPresentGrid(vibrate)
-        
         pygame.display.update()
 
         res = checkWon()
         if res < 9999:
             gameOver(res)
 
-        clock.tick(20)
+        clock.tick(20)    
     
-# Initialization of Pygame
-#close()
-pygame.init()
-clock = pygame.time.Clock()
-
-# Initializing the Screen
-horizontal_blocks=6
-vertical_blocks=11
-blocks = 40
-width = blocks*horizontal_blocks
-height = blocks*vertical_blocks
-display = pygame.display.set_mode((width, height))
-
-# Colors
-background = (21, 67, 96)
-border = (208, 211, 212)
-red = (231, 76, 60)
-white = (244, 246, 247)
-violet = (136, 78, 160)
-yellow = (244, 208, 63)
-green = (88, 214, 141)
-
-#Selected players
-playerColor = [red, green]
-noPlayers = 2
-
-font = pygame.font.SysFont("Times New Roman", 20)
-
-#title of the game window 
-pygame.display.set_caption("Chain Reaction %d Player, Isis vs AI" % noPlayers)
-
-#initialize players and scores
-score = []
-players = []
-for i in range(noPlayers):
-    score.append(0)
-    players.append(playerColor[i])
-
-
-d = blocks//2 - 2
-
-cols = int(width//blocks)
-rows = int(height//blocks)
-grid = []
-
-
 
 def GameIA():
-
     global grid
     initializeGrid()
-
     loop = True
-
     turns = 0
-    
     currentPlayer = 0
-
     vibrate = .5
-
     operators = [(i,j) for i,f in enumerate(grid) for j,c in enumerate(f)]
-
     node = NodeChain(player=(88, 214, 141),value="inicio",state = grid, operators= operators)
-
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -300,20 +283,16 @@ def GameIA():
                 currentPlayer += 1
                 if currentPlayer >= noPlayers:
                     currentPlayer = 0
-        
+
         display.fill(background)
         # Vibrate the Atoms in their Cells
         vibrate *= -1
-        
         drawGrid(currentPlayer)
         showPresentGrid(vibrate)
-        
         pygame.display.update()
-
         res = checkWon()
         if res < 9999:
             gameOver(res)
-
         clock.tick(20)
     
-GameIA()
+game2()
